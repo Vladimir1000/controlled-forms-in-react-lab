4a15d4278df948d8b6c6a07cde2cd8c55fef2e70
Vladimir1000 <vladimir.dzelovski@gmail.com>
2024-06-27 07:19:40 -0500

    Controlled Forms in React Lab

---
+++ controlled-forms-in-react-lab/src/App.jsx
@@ -0,0 +1,13 @@
+import BookShelf from './components/BookShelf'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>My Bookshelf</h1>
+      <BookShelf />
+    </div>
+  )
+}
+
+export default App
